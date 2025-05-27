f8806e54ce5f042f59b5f2f3b5aa4e0db2d463f7
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-05-27 17:18:23 +0300

    update send message route

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/conversations', [ConversationController::class, 'store']);
     Route::get('/conversations', [ConversationController::class, 'index']);
 
-    Route::post('/conversations/messages/{conversation}', [MessageController::class, 'send']);
+    Route::post('/conversations/{conversation}/messages/', [MessageController::class, 'send']);
     Route::get('/conversations/{conversation}/messages', [MessageController::class, 'fetch']);
     Route::post('/conversations/{conversation}/read', [MessageController::class, 'markAsRead']);
 });
